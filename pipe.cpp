c9fa516007f3a0af132f1764f2082fb69beab673
Rujuta <rujuta.phaltankar@gmail.com>
2026-09-04 00:30:22 +0530

    Changes in codes

--- pipe.cpp
+++ pipe.cpp
@@ -5,32 +5,29 @@
 
 using namespace std;
 
-int main() {
+int main () {
     int fd[2];
     
     pid_t pid = fork();
-
-    if (pid < 0) {
+    
+    if(pid < 0) {
         perror("fork");
         return 1;
     }
-
-    if (pid > 0) {
+    
+    if(pid > 0) {
         close(fd[0]);
-        const char *message = "Hello from Parent!";
-        write(fd[1], message, strlen(message) + 1);
+        const char *message = "Hello from parent";
+        write(fd[1],message,strlen(message) + 1);
         close(fd[1]);
         wait(NULL);
-        cout << "Parent: Message sent successfully." << endl;
     }
-
     else {
         close(fd[1]);
         char buffer[100];
-        read(fd[0], buffer, sizeof(buffer));
-        cout << "Child received: " << buffer << endl;
+        read(fd[0],buffer,sizeof(buffer));
+        cout << "\nChild received: " << buffer << endl;
         close(fd[0]);
     }
-
     return 0;
 }
